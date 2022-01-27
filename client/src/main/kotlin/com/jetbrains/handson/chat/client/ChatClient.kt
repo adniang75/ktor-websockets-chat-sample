f3cc70fd974141193d3cbfbd5e4db975773cfb96
Alassane NIANG <adniang75@gmail.com>
2022-01-27 23:11:19 +0100

    Creating a websocket chat application

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -4,10 +4,47 @@ import io.ktor.client.*
 import io.ktor.client.features.websocket.*
 import io.ktor.http.*
 import io.ktor.http.cio.websocket.*
-import io.ktor.util.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-@KtorExperimentalAPI
 fun main() {
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "localhost", port = 8080, path = "/chat") {
+            val messageOutputRoutine = launch { outputMessages() }
+            val userInputRoutine = launch { inputMessages() }
 
+            userInputRoutine.join() // wait for completion; either "exit" or "error"
+            messageOutputRoutine.cancelAndJoin()
+        }
+    }
+    client.close()
+    println("Connection closed. Goodbye!")
+}
+
+suspend fun DefaultWebSocketSession.outputMessages() {
+    try {
+        for (message in incoming) {
+            message as? Frame.Text ?: continue
+            println(message.readText())
+        }
+    } catch (exception: Exception) {
+        println("Error while receiving: ${exception.localizedMessage}")
+    }
+}
+
+suspend fun DefaultWebSocketSession.inputMessages() {
+    while (true) {
+        val message = readLine() ?: ""
+        if (message.equals("exit", true)) return
+        try {
+            send(message)
+        } catch (exception: Exception) {
+            println("Error while sending: ${exception.localizedMessage}")
+            return
+        }
+    }
 }
